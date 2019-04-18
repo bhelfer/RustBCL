6cf2f4fa17dd1880834a5004ed8ac7ba3849aba7
SKYSCRAPERS1999 <386333667@qq.com>
2019-04-18 00:10:41 -0700

    Change .gitignore

--- src/container.rs
+++ src/container.rs
@@ -12,7 +12,7 @@ pub struct serializer<T> {
     pub refer_type: PhantomData<T>
 }
 
-impl serializer<char> {
+impl serializer<String> {
     pub fn serialize(data: Box<String>) -> serial_ptr<char> {
         let data = Box::leak(data);
         let mut ptr: serial_ptr<char> = serial_ptr::new(data.len());
