f7e0836e5719a80612692838da44a2532edda466
SKYSCRAPERS1999 <386333667@qq.com>
2019-04-18 00:08:22 -0700

    simplify

--- src/array.rs
+++ src/array.rs
@@ -9,7 +9,6 @@ use std::marker::PhantomData;
 use global_pointer::GlobalPointer;
 use shmemx::shmem_broadcast64;
 
-
 pub struct Array<T>{
     pub local_size: usize,
     pub ptrs: Vec<GlobalPointer<T>>,
@@ -27,38 +26,15 @@ impl <'a, T: Clone> Array<T> {
     */
     pub fn init(config: &mut Config, n:usize) -> Array<T> {
         let local_size = (n + shmemx::n_pes() - 1) / config.rankn;
-//        let ptrs = Vec(<GlobalPointer<T>>);
         let mut ptrs = vec!(GlobalPointer::null(); config.rankn);
-        //self.ptrs[shmemx::my_pe()] = c::alloc<T>(self.local_size);
         ptrs[config.rank] = config.alloc::<T>(local_size);
-        //self.local_size = (n + BCL::nprocs() - 1) / BCL::nprocs();
-        //self.ptrs[BCL::rank()] = BCL::alloc<char>(self.local_size);
 
         for rank in 0..config.rankn {
-//            let mut ptr1: GlobalPointer<T> = self.ptrs[rank];
-//            comm::broadcast(&mut ptr1, rank);
             comm::broadcast(&mut ptrs[rank], rank);
         }
-//        let refer_type = PhantomData;
-//        Array{ local_size, ptrs, refer_type}
-        Array{ local_size, ptrs}
+        Array {local_size, ptrs}
     }
-//    pub fn array(&mut self, n: usize) {
-//        self.local_size = (n + shmemx::n_pes() - 1) / shmemx::n_pes();
-//        //let mut c: Config = config::Config{};
-//        let mut c: Config = Config::init(n);
-//
-//        //self.ptrs[shmemx::my_pe()] = c::alloc<T>(self.local_size);
-//        self.ptrs[shmemx::my_pe()] = c.alloc::<T>(self.local_size);
-//        //self.local_size = (n + BCL::nprocs() - 1) / BCL::nprocs();
-//        //self.ptrs[BCL::rank()] = BCL::alloc<char>(self.local_size);
-//
-//        for rank in 0..shmemx::n_pes() {
-//            let mut ptr1: GlobalPointer<T> = self.ptrs[rank];
-//            comm::broadcast(&mut ptr1, rank);
-//            //self.ptrs[rank] = BCL::broadcast(self.ptrs[rank], rank);
-//        }
-//    }
+
     pub fn read(&self, idx: usize) -> T {
         let rank: usize = idx / self.local_size;
         if rank > shmemx::n_pes() {
@@ -66,10 +42,8 @@ impl <'a, T: Clone> Array<T> {
         }
         let local_idx: usize = idx % self.local_size; // mod % is enough
         return self.ptrs[rank].rget();
-        //return self.ptrs[rank]
-        //return self.ptrs[rank][local_idx]
-        
     }
+
     pub fn write(&mut self, c: T, idx: usize) {
         let rank: usize = idx / self.local_size;
         if rank > shmemx::n_pes() {
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod array;
 use config::Config;
 use global_pointer::GlobalPointer;
 use array::Array;
+
 fn main() {
     let mut config = Config::init(1);
 	let rankn = config.rankn;
@@ -15,41 +16,6 @@ fn main() {
         config.finalize();
         return;
     }
-    let mut ptr1 = GlobalPointer::null();
-    if config.rank == 0 {
-        ptr1 = config.alloc::<i32>(rankn);
-    }
-    println!("my_rank: {}, ptr before broadcast: {:?}", config.rank, ptr1);
-    comm::broadcast(&mut ptr1, 0);
-    println!("my_rank: {}, ptr after broadcast: {:?}", config.rank, ptr1);
-
-    // write value
-    (ptr1 + config.rank).rput(config.rank as i32);
-    config.barrier();
-
-    let mut value;
-    if config.rank == 0 {
-        let p1 = ptr1.local();
-        let p_slice = unsafe{ std::slice::from_raw_parts(p1, config.rankn) };
-        println!("Rank 0 Sees: ");
-        for i in 0..config.rankn {
-            value = p_slice[i];
-            println!("{}: {}", i, value);
-        }
-    }
-    config.barrier();
-    // barrier not work TaT, or just println! is slow?
-    println!("barrier1, rank{}!", config.rank);
-    config.barrier();
-
-    if config.rank == 1 {
-        println!("Rank 1 Sees: ");
-        for i in 0..config.rankn {
-            value = (ptr1 + i).rget();
-            println!("{}: {}", i, value);
-        }
-    }
-    config.barrier();
 
     let mut arr = Array::<char>::init(&mut config, rankn);
 //    arr.array(100);  // what does this method do?
@@ -61,8 +27,5 @@ fn main() {
         }
     }
 
-    if config.rank == 0 {
-        config.free(ptr1);
-    }
     config.finalize();
 }
