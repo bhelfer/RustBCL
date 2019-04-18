6399794651e952843500fefc5042be5b91b74d94
yhZhai <862989569@qq.com>
2019-04-18 13:04:49 -0700

    check point

--- src/queue.rs
+++ src/queue.rs
@@ -1,2 +1,24 @@
 #![allow(dead_code)]
 #![allow(unused)]
+use global_pointer;
+use comm;
+use config;
+use config::Config;
+use shmemx;
+use std::marker::PhantomData;
+use global_pointer::GlobalPointer;
+use array::Array;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Queue<T> {
+    pub size: usize,
+    pub refer_type: PhantomData<T>,
+    array: Array<T>,
+    layer_size: usize,
+}
+
+impl<'a, T> Queue<T> {
+    pub fn init() -> Queue<T> {
+        let queue = Queue{size: 0, refer_type: PhantomData};
+    }
+}
