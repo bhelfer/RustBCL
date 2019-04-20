6e792e4fdcfdb397dccb25e6bc68fc8f07992482
SKYSCRAPERS1999 <386333667@qq.com>
2019-04-19 17:06:52 -0700

    Add testing header

--- src/main.rs
+++ src/main.rs
@@ -21,18 +21,65 @@ fn main() {
         return;
     }
 
-//    array part
+    // ----------- jack's part ------------
+    if config.rank == 0 { println!("------------Jack's test------------\n"); }
 
-//    let mut arr = Array::<char>::init(&mut config, rankn);
-//    arr.write(('a' as u8 + config.rank as u8) as char, config.rank);
-//    config.barrier();
-//    if config.rank == 0 {
-//        for i in 0..config.rankn {
-//            println!("{}: {}", i, arr.read(i));
-//        }
-//    }
+    let mut ptr1 = GlobalPointer::null();
+    if config.rank == 0 {
+        let rankn = config.rankn;
+        ptr1 = config.alloc::<i32>(rankn);
+    }
+//    println!("my_rank: {}, ptr before broadcast: {:?}", config.rank, ptr1);
+    comm::broadcast(&mut ptr1, 0);
+//    println!("my_rank: {}, ptr after broadcast: {:?}", config.rank, ptr1);
 
-    // hashtable part
+    // write value
+    (ptr1 + config.rank).rput(config.rank as i32);
+    config.barrier();
+
+    let mut value;
+    if config.rank == 0 {
+        let p1 = ptr1.local();
+        let p_slice = unsafe{ std::slice::from_raw_parts(p1, config.rankn) };
+        println!("Rank 0 Sees: ");
+        for i in 0..config.rankn {
+            value = p_slice[i];
+            println!("{}: {}", i, value);
+        }
+    }
+    config.barrier();
+    // barrier not work TaT, or just println! is slow?
+    println!("barrier1, rank{}!", config.rank);
+    config.barrier();
+
+    if config.rank == 1 {
+        println!("Rank 1 Sees: ");
+        for i in 0..config.rankn {
+            value = (ptr1 + i).rget();
+            println!("{}: {}", i, value);
+        }
+    }
+    config.barrier();
+
+    if config.rank == 0 {
+        config.free(ptr1);
+    }
+
+    // ----------- array's part ------------
+    if config.rank == 0 { println!("\n\n------------Array's test------------\n"); }
+
+    let mut arr = Array::<char>::init(&mut config, rankn);
+    arr.write(('a' as u8 + config.rank as u8) as char, config.rank);
+    config.barrier();
+    if config.rank == 0 {
+        for i in 0..config.rankn {
+            println!("{}: {}", i, arr.read(i));
+        }
+    }
+
+    // ----------- HashTable's part ------------
+    if config.rank == 0 { println!("\n\n------------HashTable's test------------\n"); }
+
     let mut hash_table = HashTable::<usize, char>::new(&mut config, 1024);
     config.barrier();
 
