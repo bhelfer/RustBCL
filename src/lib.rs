f66b8aa22bcbf13a850f2fabcbbc65434ee920a8
SKYSCRAPERS1999 <386333667@qq.com>
2019-04-26 16:43:50 -0700

    Remove test in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -10,27 +10,28 @@ pub mod utils;
 #[cfg(test)]
 mod tests {
 
-    use config::Config;
-    use array::Array;
-    use comm;
+//    use config::Config;
+//    use array::Array;
+//    use comm;
 
-    #[test]
-    fn it_works() {
-        let mut config = Config::init(1);
-        let rankn = config.rankn;
+//    #[test]
+//    fn it_works() {
+//        let mut config = Config::init(1);
+//        let rankn = config.rankn;
+//
+//        if config.rank == 0 { println!("\n\n------------Array's test------------\n"); }
+//        let rankn = config.rankn;
+//
+//        let mut arr = Array::<char>::init(&mut config, rankn);
+//        arr.write(('a' as u8 + config.rank as u8) as char, config.rank);
+//
+//        comm::barrier();
+//
+//        if config.rank == 0 {
+//            for i in 0..config.rankn {
+//                println!("{}: {}", i, arr.read(i));
+//            }
+//        }
+//    }
 
-        if config.rank == 0 { println!("\n\n------------Array's test------------\n"); }
-        let rankn = config.rankn;
-
-        let mut arr = Array::<char>::init(&mut config, rankn);
-        arr.write(('a' as u8 + config.rank as u8) as char, config.rank);
-
-        comm::barrier();
-
-        if config.rank == 0 {
-            for i in 0..config.rankn {
-                println!("{}: {}", i, arr.read(i));
-            }
-        }
-    }
 }
