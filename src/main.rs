dcaddaa7c4adbfa2d9eafa81d5a9853334d7f7bf
Dilnoza Bobokalonova <dilnoza1@berkeley.edu>
2019-05-07 17:43:44 -0700

    changes to array

--- src/main.rs
+++ src/main.rs
@@ -173,25 +173,19 @@ fn test_global_pointer(config: &mut Config) {
 }
 
 fn test_array(config: &mut Config) {
-    //let args: Vec<String> = env::args().collect();
-    //if args.len() <= 1 { panic!("not enough arguments"); }
 
     let rankn: i32 = config.rankn as i32;
     let rank: i32 = config.rank as i32;
-    
-    //let n: i32 = args[1].clone().parse().unwrap();
-    let n = 120;
-    let size_array = (n*rankn) as usize;
-    let workload = 131072 * rankn;
-    let iters = workload as usize / size_array;
-//    println!("n, rankn, rank = ({}, {}, {})", n, rankn, rank);
-    let mut arr = Array::<i64>::init(config, size_array);
+    let array_size = 1024;
+//    let total_workload = 131072; // strong scaling
+    let total_workload = 131072 * config.rankn; // weak scaling
+    let local_workload = (total_workload + config.rankn - 1) / config.rankn;
+    let mut arr = Array::<i64>::init(config, array_size);
     let mut rng: StdRng = SeedableRng::from_seed([rankn as u8; 32]);
-    /*for i in 0..(n*rankn)  {
-        arr.write(0 as i64, i);
-    }*/
-    for i in rank..(rank+n) {
-        arr.write(0 as i64, i as usize);
+    if config.rank == 0 {
+        for i in 0.. array_size {
+            arr.write(0 as i64, i as usize);
+        }
     }
     comm::barrier();
     let mut time1: time::Tm = time::now();
@@ -200,18 +194,15 @@ fn test_array(config: &mut Config) {
     if config.rank == 0 {
         time1 = time::now();
     }
-    for i in 0..iters {
-        for j in 0..size_array {
-            //let mut ptr = arr.get_ptr(j);
-            let mut ptr = arr.get_ptr(rng.gen_range(0, size_array as i32) as usize);
-            comm::long_atomic_fetch_add(&mut ptr, 1 as i64);
-        }
+    for i in 0..local_workload{
+        let mut ptr = arr.get_ptr(rng.gen_range(0, array_size as i32) as usize);
+        comm::long_atomic_fetch_add(&mut ptr, 1 as i64);
     }
     comm::barrier();
     if config.rank == 0 {
         time2 = time::now();
         time_res = time2 - time1;
-        for i in 0..size_array {
+        for i in 0..array_size {
             println!("{}: {}", i, arr.read(i));
         }
         println!("time is {:?}", time_res);
